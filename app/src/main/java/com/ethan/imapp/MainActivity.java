3d8c07804b9fb7922e3edb9918e3c751aa226ead
LCN1993 <snilove@hotmail.com>
2019-12-20 18:12:20 +1100

    Added: outerlayer encapsulation

--- app/src/main/java/com/ethan/imapp/MainActivity.java
+++ app/src/main/java/com/ethan/imapp/MainActivity.java
@@ -1,26 +1,77 @@
 package com.ethan.imapp;
 
+import android.view.View;
+import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.ViewTarget;
 import com.ethan.common.app.BaseActivity;
+import com.ethan.common.widget.PortraitView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity  {
 
-    @BindView(R.id.txt_text)
-    TextView mTextView;
+    @BindView(R.id.appBar)
+    View mLayAppBar;
+
+    @BindView(R.id.img_portrait)
+    PortraitView mPortrait;
+
+    @BindView(R.id.txt_title)
+    TextView mTitle;
+
+    @BindView(R.id.lay_container)
+    FrameLayout mContainer;
+
+    @BindView(R.id.navigation)
+    BottomNavigationView mNavigation;
+
 
     @Override
     protected int getContentLayoutId() {
         return R.layout.activity_main;
     }
 
+
     @Override
     protected void initWidget() {
         super.initWidget();
-        mTextView.setText("Hello Kitty");
+        Glide.with(this)
+                .load(R.drawable.bg_src_morning)
+                .centerCrop()
+                .into(new ViewTarget<View, GlideDrawable>(mLayAppBar) {
+                    @Override
+                    public void onResourceReady(GlideDrawable resource, GlideAnimation<? super GlideDrawable> glideAnimation) {
+                        this.view.setBackground(resource.getCurrent());
+                    }
+                })
+        ;
+
+
     }
+
+    @Override
+    protected void initData() {
+        super.initData();
+    }
+
+    @OnClick(R.id.img_search)
+    void onSearchMenuClick(){
+
+    }
+
+    @OnClick({R.id.btn_action})
+    void onActionClick(){
+
+    }
+
 }
 
 
